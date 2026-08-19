d64874eff8b2eb62a252aa3db6853085ead76085
Vishwanath9973 <bhadragondv@gmail.com>
2026-08-19 22:49:20 +0530

    Create add.java

---
+++ day1/add.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class add {
+
+	public static void main(String[] args) {
+		 int number1 = 10;
+	     int number2 = 20;
+
+	     int sum = number1 + number2;
+
+	     System.out.println("Sum = " + sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
