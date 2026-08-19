0eb47b3f3c2ee7418d7abe0a2a063a3903bdafe9
Vishwanath9973 <bhadragondv@gmail.com>
2026-08-19 23:11:55 +0530

    Create ex2.java

---
+++ day3/ex2.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class exc2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		try {
+			System.out.println(10 / 0);
+
+
+		} catch (ArithmeticException e) {
+			System.out.println(" arthi handle");
+		} 
+		System.out.println("Hello sir");
+	}
+		// TODO Auto-generated method stub
+
+	}
