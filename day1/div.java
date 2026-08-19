54b87e0f806fee554a8b63d6b26c8035bba17dcc
Vishwanath9973 <bhadragondv@gmail.com>
2026-08-19 22:52:14 +0530

    Create div.java

---
+++ day1/div.java
@@ -0,0 +1,16 @@
+[8:17 pm, 19/08/2026] Vishwanath Bhadragond: package demo;
+
+public class div {
+
+	public static void main(String[] args) {
+		 int number1 = 20;
+	     int number2 = 20;
+
+	     int div = number1 / number2;
+
+	     System.out.println("div = " + div);
+		// TODO Auto-generated method stub
+
+	}
+
+}
