82a4fb65b0620381a82db1fc5f760af306c7e925
Vishwanath9973 <bhadragondv@gmail.com>
2026-09-01 16:22:13 +0530

    Add files via upload

---
+++ day9/jumpgame.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class jumpgame {
+
+	public static void main(String[] args) {
+		int[] nums = {2,1,1,0,4};
+		int maxReach=0;
+		for(int i = 0; i < nums.length; i++) {
+			if(i> maxReach) {
+				break;
+			}
+			maxReach = Math.max(maxReach,  i + nums[i]);
+			
+		}
+		if(maxReach>= nums.length-1) {
+			System.out.println("Can reach the last position");
+		}else {
+			System.out.println("Cannot reach the last position");
+		}
+
+
+
+	}
+
+}
